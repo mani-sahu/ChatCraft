72520457a93628d4f3ced512806100b8b64bd525
mani-sahu <manisahu0912@gmail.com>
2023-11-20 05:49:54 +0530

    Stashed changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDDxmxFooox5qrJGOn9OGpMcXB0dkK7R74",
+  authDomain: "chatcraft-42d6f.firebaseapp.com",
+  projectId: "chatcraft-42d6f",
+  storageBucket: "chatcraft-42d6f.appspot.com",
+  messagingSenderId: "333128073430",
+  appId: "1:333128073430:web:1c10ee0be71aea4f100977",
+  measurementId: "G-5C2H7302F1"
+  },
+  production: true,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDDxmxFooox5qrJGOn9OGpMcXB0dkK7R74",
+  authDomain: "chatcraft-42d6f.firebaseapp.com",
+  projectId: "chatcraft-42d6f",
+  storageBucket: "chatcraft-42d6f.appspot.com",
+  messagingSenderId: "333128073430",
+  appId: "1:333128073430:web:1c10ee0be71aea4f100977",
+  measurementId: "G-5C2H7302F1"
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
